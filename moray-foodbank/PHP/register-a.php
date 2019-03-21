2469bb9ad97e074de1225d50c69c9e5b6e988b34
FireHunter3958 <firearrows@hotmail.co.uk>
2019-03-21 13:55:04 +0000

    no message

--- moray-foodbank/PHP/register-a.php
+++ moray-foodbank/PHP/register-a.php
@@ -1,34 +1,56 @@
 <?php
-$title = "Registration - Step One";
-include("header.php");
+//include("includes/header.php");
 
-if(isset($_POST['submit'])){
-	$_SESSION['title'] = $_POST['title'];
-	$_SESSION['fname'] = $_POST['firstname'];
-	$_SESSION['lname'] = $_POST['lname'];
-	$_SESSION['email'] = $_POST['email'];
-	$_SESSION['line1'] = $_POST['line1'];
-	$_SESSION['line2'] = $_POST['line2'];
-	$_SESSION['city'] = $_POST['address3'];
-	$_SESSION['postcode'] = $_POST['postcode'];
-	$_SESSION['phone'] = $_POST['phoneNo'];
-	$_SESSION['dofb'] = $_POST['dofb'];
-	
-	$_SESSION['cType'] = $_POST['contactType'];
-	$_SESSION['cName'] = $_POST['contactName'];
-	$_SESSION['cPhone'] = $_POST['contactPhone'];
-	$_SESSION['relationship'] = $_POST['contactRel'];
+session_start();
 
-	
-	header("Location: register-b.php");
+if(!isset($_SESSION['Username'])){
+	header("Location: ../index.php");
 }
+if(isset($_POST['submit'])){
+	if(isset($_POST['title'])) {
+		if(isset($_POST['firstName'])) {
+			if(isset($_POST['lname'])) {
+			
+				$_SESSION['title'] = $_POST['title'];
+				$_SESSION['fname'] = $_POST['firstName'];
+				$_SESSION['lname'] = $_POST['lname'];
+				$_SESSION['email'] = $_POST['email'];
+				$_SESSION['line1'] = $_POST['line1'];
+				$_SESSION['line2'] = $_POST['line2'];
+				$_SESSION['city'] = $_POST['address3'];
+				$_SESSION['postcode'] = $_POST['postcode'];
+				$_SESSION['phone'] = $_POST['phoneNo'];
+				$_SESSION['dofb'] = $_POST['dofb'];
+				
+				$_SESSION['cType'] = $_POST['contactType'];
+				$_SESSION['cName'] = $_POST['contactName'];
+				$_SESSION['cphone'] = $_POST['contactPhone'];
+				$_SESSION['relationship'] = $_POST['contactRel'];
 
-if(isset($_POST['cancel'])){
-	header("Location: home.php");
-	
+				
+				header("Location: register-b.php");
+			}
+		}
+	}
 }
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Moray Foodbank</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" media="screen" href="../CSS/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="../CSS/styles.css" />
+
+</head>
+
+<body>
 <main class="container.fluid">
 <div class="container.fluid hero-wrapper">
     <div class="row hero-container">
@@ -71,51 +93,51 @@ if(isset($_POST['cancel'])){
   <div class="form-row">
     <div class="form-group col-md-2">
       <label for="title">TITLE</label>
-      <input type="text" class="form-control" name="title" id="title" placeholder="Title" value="<?php if(isset($_SESSION['title'])){echo $_SESSION['title'];} ?>">
+      <input type="text" class="form-control" id="title" placeholder="Title" required>
     </div>
     <div class="form-group col-md-4">
       <label for="firstName">FIRST NAME</label>
-      <input type="text" class="form-control" name="firstname" id="firstName" placeholder="First Name" value="<?php if(isset($_SESSION['fname'])){echo $_SESSION['fname'];} ?>">
+      <input type="text" class="form-control" id="firstName" placeholder="First Name" required>
     </div>
     <div class="form-group col-md-6">
       <label for="lname">SURNAME</label>
-      <input type="text" class="form-control" name="lname" id="lname" placeholder="Surname" value="<?php if(isset($_SESSION['lname'])){echo $_SESSION['lname'];} ?>">
+      <input type="text" class="form-control" id="lname" placeholder="Surname" required>
     </div>
   </div>
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="email">EMAIL</label>
-      <input type="text" class="form-control" name="email" id="email" placeholder="Email address" value="<?php if(isset($_SESSION['email'])){echo $_SESSION['email'];} ?>">
+      <input type="email" class="form-control" id="email" placeholder="Email address">
     </div>
     <div class="form-group col-md-6">
       <label for="line1">ADDRESS LINE 1</label>
-      <input type="text" class="form-control" name="line1" id="line1" placeholder="First line of address" value="<?php if(isset($_SESSION['line1'])){echo $_SESSION['line1'];} ?>">
+      <input type="text" class="form-control" id="line1" placeholder="First line of address">
     </div>
   </div>
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="phoneNo">PHONE</label>
-      <input type="text" class="form-control" name="phoneNo" id="phoneNo" placeholder="Phone number" value="<?php if(isset($_SESSION['phone'])){echo $_SESSION['phone'];} ?>">
+      <input type="text" class="form-control" id="phoneNo" placeholder="Phone number">
     </div>
     <div class="form-group col-md-6">
       <label for="line2">ADDRESS LINE 2</label>
-      <input type="text" class="form-control" name="line2" id="line2" placeholder="Second line of address" value="<?php if(isset($_SESSION['line2'])){echo $_SESSION['line2'];} ?>">
+      <input type="text" class="form-control" id="line2" placeholder="Second line of address">
     </div>
   </div>
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="dofb">DATE OF BIRTH</label>
-      <input type="date" class="form-control" name="dofb" id="dofb" placeholder="Date of birth" value="<?php if(isset($_SESSION['dofb'])){echo $_SESSION['dofb'];} ?>">
+      <input type="date" class="form-control" id="dofb" placeholder="Date of birth">
     </div>
     <div class="form-group col-md-6">
       <label for="address3">CITY</label>
-      <input type="text" class="form-control" name="address3" id="address3" placeholder="City" value="<?php if(isset($_SESSION['city'])){echo $_SESSION['city'];} ?>">
+      <input type="text" class="form-control" id="address3" placeholder="City">
     </div>
   </div>
   <div class="form-row">
     <div class="form-group col-md-6 offset-md-6">
       <label for="postcode">POSTCODE</label>
-      <input type="text" class="form-control" name="postcode" id="postcode" placeholder="Postcode" value="<?php if(isset($_SESSION['postcode'])){echo $_SESSION['postcode'];} ?>">
+      <input type="text" class="form-control" id="postcode" placeholder="Postcode">
     </div>
   </div>
   <br>
@@ -123,30 +145,29 @@ if(isset($_POST['cancel'])){
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="contactName">FULL NAME</label>
-      <input type="text" class="form-control" name="contactName" id="contactName" placeholder="Full name" value="<?php if(isset($_SESSION['cName'])){echo $_SESSION['cName'];} ?>">
+      <input type="text" class="form-control" id="contactName" placeholder="Full name">
     </div>
     <div class="form-group col-md-6">
       <label for="contactPhone">PHONE</label>
-      <input type="text" class="form-control" name="contactPhone" id="contactPhone" placeholder="Phone Number" value="<?php if(isset($_SESSION['cPhone'])){echo $_SESSION['cPhone'];} ?>">
+      <input type="text" class="form-control" id="contactPhone" placeholder="Phone Number">
     </div>
   </div>
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="contactRel">RELATIONSHIP</label>
-      <input type="text" class="form-control" name="contactRel" id="contactRel" placeholder="Relationship to volunteer" value="<?php if(isset($_SESSION['relationship'])){echo $_SESSION['relationship'];} ?>">
+      <input type="text" class="form-control" id="contactRel" placeholder="Relationship to volunteer">
     </div>
     <div class="form-group col-md-6">
       <label for="contactType">CONTACT TYPE</label>
-      <select class="form-control" id="contactType" name="contactType">
+      <select class="form-control" id="contactType">
 		<option value="0">Support</option>
-        <option value="1" <?php if((isset($_SESSION['cType'])) && $_SESSION['cType']=="1"){echo "selected";}?>>Emergency</option>
+        <option value="1">Emergency</option>
       </select>
     </div>
   </div>
   <div class="form-row">
     <div class="form-group col-md-6 offset-md-3 form-buttons">
-		<!--<button type="submit" class="btn btn-primary disabled">Back</button>-->
-		<input type ="submit" name="cancel" class ="btn btn-primary" value="Back">
+		<button type="submit" class="btn btn-primary disabled">Back</button>
 		<!--<button type="submit" class="btn btn-primary">Next</button>-->
 		<input type ="submit" name="submit" class ="btn btn-primary" value="Next">
     </div>
